875489af713dcf9e4214dd1d099852585a8d4788
weifeng-l <s10208179@connect.np.edu.sg>
2021-02-17 13:38:28 +0800

    API live data

--- js/data.js
+++ js/data.js
@@ -1,31 +1,66 @@
-    init()
+init()
 
+function init() {
+    var url = "https://api.covid19api.com/summary"
 
-    function init() {
-        var url = "https://api.covid19api.com/summary"
+    var data = ''
 
-        var data = ''
+    $.get(url, function (data) {
+        console.log(data.Global)
 
-        $.get(url, function (data) {
-            console.log(data.Global)
+        data = `
 
-            data = `
+        <td>${data.Global.TotalConfirmed}</td>
+        <td>${data.Global.TotalDeaths}</td>
+        <td>${data.Global.TotalRecovered}</td>
+        
+        `
 
-            <td>${data.Global.TotalConfirmed}</td>
-            <td>${data.Global.TotalDeaths}</td>
-            <td>${data.Global.TotalRecovered}</td>
-            
-            `
-
-            $("#data").html(data)
-        })
-    }
+        $("#data-all").html(data)
+    })
+}
 
 
 function refreshData() {
     clearData()
     init()
 }
-    function clearData() {
-        $("#data").empty()
-    }
+function clearData() {
+    $("#data-all").empty()
+}
+
+
+var input = document.querySelector('.input_text');
+var recover = document.querySelector('.recover');
+var country = document.querySelector('.country');
+var cases = document.querySelector('.cases');
+var death = document.querySelector('.death');
+var button = document.querySelector('.submit')
+
+var myHeaders = new Headers();
+myHeaders.append("Cookie", "__cfduid=d4f2a7bbb632ebff6eb2a18470ddfb0701612834567");
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+};
+
+button.addEventListener('click', function (name) {
+    fetch("https://disease.sh/v3/covid-19/countries/" + input.value + "", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            var countryValue = result.country;
+            var casesValue = result.cases;
+            var deathValue = result.deaths;
+            var recoverValue = result.recovered;
+
+            country.innerHTML = countryValue;
+            cases.innerHTML = casesValue;
+            death.innerHTML = deathValue;
+            recover.innerHTML = recoverValue;
+        })
+
+        .catch(error => console.log('error', error));
+
+});
